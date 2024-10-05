0093d03a7af4cc8d2d3fcdea24adebef09896dbe
rohitmulawkar <157627085+rohitmulawkar@users.noreply.github.com>
2024-10-05 13:47:10 +0530

    Add files via upload

---
+++ table.java
@@ -0,0 +1,15 @@
+public class table{
+public static void main (String [] args)
+{
+//System.out.println("Enter the number for table");
+//Scanner sc = new Scanner(System.in);
+//int j = sc.nextInt();
+System.out.println("Table of "+10+" is as follows");
+
+for (int i=1;i<11;i++)
+{
+System.out.println("10 * " + i +"= "+(10*i));
+
+}
+}
+}
